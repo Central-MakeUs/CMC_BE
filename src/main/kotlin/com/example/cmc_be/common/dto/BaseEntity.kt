45671b2b83114a4de6370bfdf0c3fb11f8ba0408
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-10-13 18:46:39 +0900

    refactor : BaseEntity 수정

--- src/main/kotlin/com/example/cmc_be/common/dto/BaseEntity.kt
+++ src/main/kotlin/com/example/cmc_be/common/dto/BaseEntity.kt
@@ -1,17 +1,12 @@
 package com.example.cmc_be.common.dto
 
-import lombok.Getter
+import jakarta.persistence.*
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
-import javax.persistence.Column
-import javax.persistence.EntityListeners
-import javax.persistence.MappedSuperclass
 
 
-@Getter
-@MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 abstract class BaseEntity {
     @Column(name = "createdAt", updatable = false)
