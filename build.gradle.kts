55f701feadc4b12d40947e3c9e5952d704d6159e
이해찬[Vertical Service Dev1] <cksgodl1@naver.com>
2023-10-13 15:52:28 +0900

    Chore : 버전 업데이트에 따른 Swagger 대응

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
