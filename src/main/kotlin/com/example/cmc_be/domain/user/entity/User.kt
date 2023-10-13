8c5926f6cca97a37175f682914c843c0eee59dfa
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-10-13 14:41:58 +0900

    refactor : 유저 엔티티 수정

--- src/main/kotlin/com/example/cmc_be/domain/user/entity/User.kt
+++ src/main/kotlin/com/example/cmc_be/domain/user/entity/User.kt
@@ -1,13 +1,15 @@
 package com.example.cmc_be.domain.user.entity
 
+import com.example.cmc_be.common.dto.BaseEntity
+import com.example.cmc_be.domain.user.enums.Generation
+import com.example.cmc_be.domain.user.enums.SignUpApprove
+import com.example.cmc_be.domain.user.enums.UserRole
 import org.hibernate.annotations.BatchSize
 import org.hibernate.annotations.DynamicInsert
 import org.hibernate.annotations.DynamicUpdate
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
-import java.time.LocalDate
-import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
@@ -15,7 +17,7 @@ import javax.persistence.*
 @DynamicUpdate
 @BatchSize(size = 100)
 @DynamicInsert
-class User : UserDetails {
+class User : UserDetails, BaseEntity()  {
     @Id @Column(name = "id") @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
     //유저 아이디
     private val username: String? = null
@@ -26,14 +28,23 @@ class User : UserDetails {
 
     val nickname: String? = null
 
-    @Column(name = "phoneNumber") val phoneNumber: String? = null
+    @Column(name = "phoneNumber")
+    val phoneNumber: String? = null
 
-    @Column(name = "role") val role: String? = null
+    @Enumerated(EnumType.STRING) val role : UserRole = UserRole.ROLE_USER
 
+    @Enumerated(EnumType.STRING) val  nowGeneration : Generation? = null
+
+
+    @Enumerated(EnumType.STRING) val  signUpApprove : SignUpApprove = SignUpApprove.NOT
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    @JoinColumn(name = "userId")
+    private val userCard: List<UserPart> = ArrayList<UserPart>()
 
     override fun getAuthorities(): Collection<GrantedAuthority>? {
         val authorities: MutableCollection<GrantedAuthority> = ArrayList()
-        for (role in role!!.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()) authorities.add(
+        for (role in role.value!!.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()) authorities.add(
             SimpleGrantedAuthority(role)
         )
         return authorities
