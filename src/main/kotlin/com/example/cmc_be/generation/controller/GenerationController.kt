0071e17f51f2d783ae6324ad66de96498245c864
이해찬[Vertical Service Dev1] <cksgodl1@naver.com>
2023-10-14 19:24:47 +0900

    rename : 주차별 모든 날짜 GET을 Admin으로 이동
    유저에게는 주차별 모든 날짜 보여주는 화면이 없음 -> 본인의 이번기수 출석조회로 보여주면 될 듯?

--- src/main/kotlin/com/example/cmc_be/generation/controller/GenerationController.kt
+++ src/main/kotlin/com/example/cmc_be/generation/controller/GenerationController.kt
@@ -1,25 +1,15 @@
 package com.example.cmc_be.generation.controller
 
-import com.example.cmc_be.common.response.CommonResponse
-import com.example.cmc_be.domain.user.entity.User
-import com.example.cmc_be.generation.dto.GenerationRes
 import com.example.cmc_be.generation.service.GenerationService
-import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/generations")
-@Tag(name ="04 Generation 기수 주차 정보 API")
+@Tag(name = "04 Generation 기수 주차 정보 API")
 class GenerationController(
     private val generationService: GenerationService
 ) {
-    @Operation(summary = "04-01 주차별 날짜 GET API")
-    @GetMapping("")
-    fun getGenerationWeeksInfo(@AuthenticationPrincipal user: User) : CommonResponse<List<GenerationRes.GenerationWeeksInfoDto>>{
-        return CommonResponse.onSuccess(generationService.getGenerationWeeksInfo(user))
-    }
+    
 }
