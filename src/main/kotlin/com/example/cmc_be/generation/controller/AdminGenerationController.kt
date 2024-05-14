0d8e7d569616ad240d234cdae70b35a5cb32c436
이해찬[Vertical Service Dev1] <cksgodl1@naver.com>
2024-05-14 21:49:06 +0900

    refactor : generation package 코틀린 스럽게 변경

--- src/main/kotlin/com/example/cmc_be/generation/controller/AdminGenerationController.kt
+++ src/main/kotlin/com/example/cmc_be/generation/controller/AdminGenerationController.kt
@@ -1,12 +1,11 @@
 package com.example.cmc_be.generation.controller
 
 import com.example.cmc_be.common.response.CommonResponse
-import com.example.cmc_be.domain.user.entity.User
-import com.example.cmc_be.generation.dto.GenerationReq
+import com.example.cmc_be.domain.generation.entity.GenerationWeeksInfo
+import com.example.cmc_be.generation.dto.PostGenerationInfoReq
 import com.example.cmc_be.generation.service.GenerationService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -18,17 +17,16 @@ class AdminGenerationController(
     @Operation(summary = "04-01 주차별 정보 POST API")
     @PostMapping
     fun postGenerationWeeksInfo(
-        @AuthenticationPrincipal user: User,
-        @RequestBody generationInfo: GenerationReq.GenerationInfo
-    ): CommonResponse<String> {
-        return CommonResponse.onSuccess(generationService.postGenerationWeeksInfo(generationInfo))
+        @RequestBody postGenerationInfoReq: PostGenerationInfoReq
+    ): CommonResponse<Unit> {
+        return CommonResponse.onSuccess(generationService.postGenerationWeeksInfo(postGenerationInfoReq))
     }
 
     @Operation(summary = "04-02 주차별 모든 날짜 GET API")
     @GetMapping("/date")
     fun getGenerationWeeksInfoDate(
-        @AuthenticationPrincipal user: User,
-    ): CommonResponse<List<String>> {
-        return CommonResponse.onSuccess(generationService.getGenerationWeeksInfoDate(user))
+        @RequestParam generation: Int,
+    ): CommonResponse<List<GenerationWeeksInfo>> {
+        return CommonResponse.onSuccess(generationService.getGenerationWeeksInfoDate(generation))
     }
 }
--- src/main/kotlin/com/example/cmc_be/generation/dto/GenerationReq.kt
+++ src/main/kotlin/com/example/cmc_be/generation/dto/GenerationReq.kt
@@ -1,14 +1,26 @@
 package com.example.cmc_be.generation.dto
 
+import com.example.cmc_be.domain.generation.entity.GenerationWeeksInfo
 import com.fasterxml.jackson.annotation.JsonFormat
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
 
-class GenerationReq {
-    data class GenerationInfo(
-        val generation: Int,
-        val week: Int,
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-        val date: LocalDate,
-        val isOffline: Boolean
-    )
+data class PostGenerationInfoReq(
+    @Schema(example = "15")
+    val generation: Int,
+    @Schema(example = "1")
+    val week: Int,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    val date: LocalDate,
+    val isOffline: Boolean
+) {
+
+    fun toEntity(): GenerationWeeksInfo {
+        return GenerationWeeksInfo(
+            generation = this.generation,
+            week = this.week,
+            date = this.date,
+            isOffline = this.isOffline
+        )
+    }
 }
