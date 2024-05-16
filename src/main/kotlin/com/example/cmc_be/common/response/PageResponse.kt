46f029b361d8ea9fc932273df711dbfb49333eec
이해찬[Vertical Service Dev1] <cksgodl1@naver.com>
2024-05-16 11:25:29 +0900

    feat : PageResponse 팩토리 함수 추가

--- src/main/kotlin/com/example/cmc_be/common/response/PageResponse.kt
+++ src/main/kotlin/com/example/cmc_be/common/response/PageResponse.kt
@@ -1,6 +1,7 @@
 package com.example.cmc_be.common.response
 
 import io.swagger.v3.oas.annotations.media.Schema
+import org.springframework.data.domain.Page
 
 
 @Schema(description = "페이징 처리 응답")
@@ -9,8 +10,21 @@ data class PageResponse<T>(
     val isLast: Boolean = true,
 
     @Schema(description = "총 요소 갯수", required = true, example = "10")
-    val totalCnt: Long = 0,
+    val totalCnt: Int = 0,
 
     @Schema(description = "요소", required = true)
     val contents: List<T> = emptyList()
-)
+) {
+
+    companion object {
+
+        fun <T, R> from(page: Page<T>, contentMapper: (T) -> R): PageResponse<R> {
+            return PageResponse(
+                isLast = page.isLast,
+                totalCnt = page.count(),
+                contents = page.toList().map { contentMapper(it) }
+            )
+        }
+
+    }
+}
