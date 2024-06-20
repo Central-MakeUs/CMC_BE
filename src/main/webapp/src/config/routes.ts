dd6a8d770f96039809c42148f3017d94f501fcde
이해찬[Vertical Service Dev1] <cksgodl1@naver.com>
2024-06-20 23:24:28 +0900

    fix : 새로고침해도 안튕기도록 변경

--- src/main/webapp/src/config/routes.ts
+++ src/main/webapp/src/config/routes.ts
@@ -3,18 +3,19 @@ import React from 'react';
 const Dashboard = React.lazy(() => import('../pages/dashboard/Dashboard'));
 
 const DemoList = React.lazy(() => import('../pages/demo/List'));
+const QRCodeList = React.lazy(() => import('../pages/attendance/QRCodeList'));
 const UserList = React.lazy(() => import('../pages/user/UserList'));
 const DemoDetail = React.lazy(() => import('../pages/demo/Detail'));
 const TextEditor = React.lazy(() => import('../pages/demo/EditorPage'));
 
 const routes = [
-  {path: '/dashboard', component: Dashboard},
-  {path: '/admin-page/dashboard', component: Dashboard},
-  {path: '/user/attendance', component: UserList},
-  {path: '/user/attendance/qrcode', component: UserList},
-  {path: '/demo/list', component: DemoList},
-  {path: '/demo/editor', component: TextEditor},
-  {path: '/demo/detail/:id', component: DemoDetail},
+    {path: '/dashboard', component: Dashboard},
+    {path: '/admin-page/dashboard', component: Dashboard},
+    {path: '/user/attendance', component: UserList},
+    {path: '/user/attendance/qrcode', component: QRCodeList},
+    {path: '/demo/list', component: DemoList},
+    {path: '/demo/editor', component: TextEditor},
+    {path: '/demo/detail/:id', component: DemoDetail},
 ];
 
 export default routes;
--- src/main/webapp/src/layout/Content/index.tsx
+++ src/main/webapp/src/layout/Content/index.tsx
@@ -5,30 +5,39 @@ import {Navigate, Route, Routes, useLocation} from 'react-router-dom';
 import routes from '../../config/routes';
 
 const loading = (
-  <div className='pt-3 text-center'>
-    <div className='sk-spinner sk-spinner-pulse'/>
-  </div>
+    <div className='pt-3 text-center'>
+        <div className='sk-spinner sk-spinner-pulse'/>
+    </div>
 );
 
 const Content = () => {
-  const location = useLocation();
-  const [isExistsFilteredRoute] = useState(routes.filter(route => route.path === location.pathname).length > 0);
+    const location = useLocation();
+    const [isExistsFilteredRoute] = useState(
+        routes.filter(route =>
+            location.pathname.startsWith("/admin-page" + route.path) ||
+            location.pathname.startsWith(route.path)
+        ).length > 0
+    );
 
-  console.log(`isExistsFilteredRoute : ${isExistsFilteredRoute}`)
-  return (
-    <Suspense fallback={loading}>
-      {!isExistsFilteredRoute ? (
-        <Navigate to='/admin-page/login'/>
-      ) : (
-        <Routes>
-          {routes.map((route, idx) => {
-            return route.component && <Route key={idx} path={route.path} element={<route.component/>}/>;
-          })}
-          <Route path='/admin-page/' element={<Navigate to='/admin-page/dashboard' replace/>}/>
-        </Routes>
-      )}
-    </Suspense>
-  );
+    routes.map((route) => {
+        console.log(route.path)
+    })
+    console.log(location.pathname)
+    console.log(`isExistsFilteredRoute : ${isExistsFilteredRoute}`)
+    return (
+        <Suspense fallback={loading}>
+            {!isExistsFilteredRoute ? (
+                <Navigate to='/admin-page/login'/>
+            ) : (
+                <Routes>
+                    {routes.map((route, idx) => {
+                        return route.component && <Route key={idx} path={route.path} element={<route.component/>}/>;
+                    })}
+                    <Route path='/admin-page/' element={<Navigate to='/admin-page/dashboard' replace/>}/>
+                </Routes>
+            )}
+        </Suspense>
+    );
 };
 
 export default React.memo(Content);
